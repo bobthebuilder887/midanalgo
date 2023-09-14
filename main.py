be3963bb16334dc3aee3488b4c894e2383d1b611
e <e@es-MacBook-Pro.local>
2023-09-14 18:31:45 +0300

    Made script assign names to each invoice batch and added back cli interface

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import argparse
+import random
 from pathlib import Path
 
 import numpy as np
@@ -8,15 +10,21 @@ import split_solver
 
 
 def match_invoices(
-    batches: dict[int, sheets.Batch], optimal: list[list[int]]
-) -> dict[int, list[int]]:
+    batches: dict[int, sheets.Batch], optimal: list[list[int]], names: list[str]
+) -> dict[str, list[int]]:
     """Match scores with corresponding invoice numbers"""
-    invoice_per_worker = {worker: [] for worker in range(len(optimal))}
 
-    for worker, scores in enumerate(optimal):
+    # Randomly assign name to each batch of invoices
+    random.shuffle(names)
+
+    # Initialize a dictionary of workers to store invoice numbers
+    invoice_per_worker = {name: [] for name in names}
+
+    # Match invoices using batch scores
+    for name, scores in zip(names, optimal):
         for score in scores:
             batches, invoices = find_matching_invoices(score, batches)
-            invoice_per_worker[worker].extend(invoices)
+            invoice_per_worker[name].extend(invoices)
 
     return invoice_per_worker
 
@@ -29,12 +37,13 @@ def find_matching_invoices(
         if batch.score == score:
             batches.pop(batch_id)
             return batches, batch.invoice_numbers
+    return batches, []
 
 
 def gen_output(
     table: pd.DataFrame,
-    matching_invoices: dict[int, list[int]],
-) -> pd.DataFrame:
+    matching_invoices: dict[str, list[int]],
+) -> pd.Series:
     # Add workers to the original table
     table["Worker"] = -1
     for worker, invoices in matching_invoices.items():
@@ -52,27 +61,64 @@ def gen_output(
     return output
 
 
-def save_output(output: pd.DataFrame, path: str | Path) -> None:
+def save_output(output: pd.Series, path: str | Path) -> None:
     output.to_excel(path)
 
 
 def main() -> None:
-    TABLE = Path("test") / "sample_data.xlsx"
-    TABLEBASE = Path("test") / "sample_tablebase.xlsx"
-    OUTPUT = Path("test") / "output.xlsx"
-    N_WORKERS = 3
+    parser = argparse.ArgumentParser(
+        prog="Midan's Sheet Processor",
+        description="Process work table",
+        epilog="All files have to be of .xslx type",
+    )
 
-    table = sheets.read_table(TABLE)
+    # Table file path
+    parser.add_argument(
+        "-t",
+        "--table",
+        help="Path to table file.",
+        type=str,
+        required=True,
+    )
+    # Base Table file path
+    parser.add_argument(
+        "-b",
+        "--table-base",
+        help="Path to table base file.",
+        type=str,
+        required=True,
+    )
+    # Output Table file path
+    parser.add_argument(
+        "-o",
+        "--output-file",
+        help="Path to output file.",
+        type=str,
+        required=True,
+    )
 
-    tablebase = sheets.read_tablebase(TABLEBASE)
+    # Parse arguments
+    args = parser.parse_args()
+
+    # TABLE = Path("test") / "sample_data.xlsx"
+    # TABLEBASE = Path("test") / "sample_tablebase.xlsx"
+    # OUTPUT = Path("test") / "output.xlsx"
+    # N_WORKERS = 3
+
+    table = sheets.read_table(args.table)
+
+    tablebase = sheets.read_tablebase(args.table_base)
+    names = sheets.read_names(args.table_base)
+
+    n_workers = len(names)
 
     batches = sheets.gen_batches(sheets.process_table(table, tablebase))
     scores = sheets.get_scores(batches)
 
     # TODO: Optimize this step
-    optimal = split_solver.get_optimal_split(scores, N_WORKERS)
+    optimal = split_solver.get_optimal_split(scores, n_workers)
 
-    expected_avg = np.sum(scores) / N_WORKERS
+    expected_avg = np.sum(scores) / n_workers
     optimal_scores = list(np.sum(split) for split in optimal)
     optimal_avg = np.mean(optimal_scores)
     optimal_var = np.var(optimal_scores)
@@ -82,13 +128,13 @@ def main() -> None:
     print(f"Obtained VAR: {optimal_var}")
     print(f"Obtained SCORES: {optimal_scores}")
 
-    matching_invoices = match_invoices(batches, optimal)
+    matching_invoices = match_invoices(batches, optimal, names)
 
     # Generate output for the output
     output = gen_output(table, matching_invoices)
 
     # Save to a specified excel file
-    save_output(output, OUTPUT)
+    save_output(output, args.output_file)
 
 
 if __name__ == "__main__":
