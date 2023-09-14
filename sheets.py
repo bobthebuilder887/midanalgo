0481658e17f017cf2cef321eb87e294535ac0807
e <e@es-MacBook-Pro.local>
2023-09-14 18:31:10 +0300

    Addded function to read worker names and some log warnings for when there's missing data

--- sheets.py
+++ sheets.py
@@ -1,3 +1,4 @@
+import logging
 from dataclasses import dataclass, field
 from pathlib import Path
 
@@ -15,12 +16,22 @@ def read_table(file_path: str | Path) -> pd.DataFrame:
 
 def read_tablebase(file_path: str | Path) -> pd.DataFrame:
     """Read the tablebase table"""
+
+    DEFAULT_SCORES = 12, 6, 4, 4, 2
+
     tablebase = pd.read_excel(file_path, header=2, usecols="B:G", index_col=0)
 
     if "DEFAULT" not in tablebase.index:
-        tablebase.loc["DEFAULT", :] = 12, 6, 4, 4, 2
+        tablebase.loc["DEFAULT", :] = DEFAULT_SCORES
+        logging.warning(f"Added DEFAULT row {DEFAULT_SCORES=} to tablebase")
 
     return tablebase
+
+
+def read_names(file_path: str | Path, sheet_name="Names") -> list[str]:
+    """Read the worker names"""
+    df = pd.read_excel(file_path, sheet_name=sheet_name)
+    return list(df["Names"].values)
 
 
 def process_table(table: pd.DataFrame, tablebase: pd.DataFrame) -> pd.DataFrame:
@@ -47,6 +58,7 @@ def process_table(table: pd.DataFrame, tablebase: pd.DataFrame) -> pd.DataFrame:
         if row["Name"] in tablebase.index:
             return tablebase.loc[row["Name"], row["Mod"]]
         else:
+            logging.warning(f"Name {row['Name']} not found! Using DEFAULT row")
             return tablebase.loc["DEFAULT", row["Mod"]]
 
     table["Score"] = table.apply(find_score, axis=1, tablebase=tablebase)
