aa06db623e8bb0dfc73ca7f576a9de56ce51daa5
e <e@es-MacBook-Pro.local>
2023-09-17 18:45:50 +0300

    Changed the solving method to a different library

--- split_solver.py
+++ split_solver.py
@@ -1,43 +1,9 @@
-from itertools import combinations
-from typing import Iterable, Sequence
-
-import numpy as np
+from numberpartitioning import karmarkar_karp
 
 
-def get_optimal_split(a: Sequence[int], n: int) -> list:
+def get_optimal_split(a: list[int], n: int) -> list[list[int]]:
     """
-    Get split indices that give the smallest variance between splits the given an array
-    and number of splits
+    Get split indices that give the smallest difference of sums between splits the given
+    an array and number of splits
     """
-    return np.split(a, find_optimal_split(a, n))
-
-
-def find_optimal_split(a: Sequence[int], n: int) -> tuple[int, ...]:
-    """
-    Find split indices that give the smallest variance between splits the given an array
-    and number of splits
-    """
-
-    # Initalize variables
-    min_var = np.sum(a)
-    idxs = gen_idxs(len(a), n)
-    optimal_idx = tuple()
-
-    for idx in idxs:
-        split = gen_split(a, idx)  # generate split with summed scores
-        split_var = np.var(split)  # calculate variance
-        if split_var < min_var:  # replace if variance smaller than minimum found
-            optimal_idx = idx
-            min_var = split_var
-
-    return optimal_idx
-
-
-def gen_idxs(size: int, n: int) -> Iterable[tuple[int]]:
-    """Generate all possible split indices given a size and number of splits"""
-    return combinations(range(1, size), n - 1)
-
-
-def gen_split(a: Iterable[int], idxs: tuple[int, ...]) -> list[int]:
-    """Generate splits with summed scores"""
-    return list(np.sum(frac) for frac in np.split(a, idxs))  # type: ignore
+    return karmarkar_karp(a, num_parts=n).partition
--- test/test_split_solver.py
+++ test/test_split_solver.py
@@ -13,22 +13,6 @@ def N() -> int:
     return 3
 
 
-def test_gen_idxs(A: list[int], N: int) -> None:
-    idxs = split_solver.gen_idxs(len(A), N)
-    assert len(next(idxs)) == N - 1  # type: ignore
-
-
-def test_gen_split(A: list[int], N: int) -> None:
-    idxs = split_solver.gen_idxs(len(A), N)
-    split = split_solver.gen_split(A, next(idxs))  # type: ignore
-    assert len(split) == N
-
-
-def test_find_optimal_split(A: list[int], N: int) -> None:
-    optimal_idx = split_solver.find_optimal_split(A, N)
-    assert len(optimal_idx) == N - 1
-
-
 def test_get_optimal_split(A: list[int], N: int) -> None:
     optimal_split = split_solver.get_optimal_split(A, N)
     assert len(optimal_split) == N
